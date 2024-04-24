1ef0ac3bc8ee4bdfcdba415be51591450d57fa68
Giovanni Andres Alzate Agudelo <119427037+AndrexMed@users.noreply.github.com>
2024-04-23 23:19:11 -0500

    Importando datos

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -2,5 +2,19 @@
 {
     public class LinqQueries
     {
+        private List<Book> booksCollection = [];
+
+        public LinqQueries()
+        {
+            using StreamReader reader = new("books.json");
+
+            string json = reader.ReadToEnd();
+            booksCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true})!;
+        }
+
+        public IEnumerable<Book> GetAllCollection()
+        {
+            return booksCollection;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CursoLinqPlatzi;
+
+LinqQueries linqQueries = new();
+void PrintValues(IEnumerable<Book> books)
+{
+    Console.WriteLine("{0, -60}, {1, 15}, {2, 15}\n", "Title", "N. Pages", "Date Post");
+    foreach (var item in books)
+    {
+        Console.WriteLine("{0, -60}, {1, 15}, {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
+
+PrintValues(linqQueries.GetAllCollection());
